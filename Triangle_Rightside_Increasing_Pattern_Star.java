1094a778af0dfd4fb3aaef2f395be99fc0931868
RajiiM <mrajalakshmiofficial@gmail.com>
2021-11-13 20:36:51 +0530

    FirstCommit

---
+++ Triangle_Rightside_Increasing_Pattern_Star.java
@@ -0,0 +1,26 @@
+//OUTPUT
+        * 
+      * * 
+    * * * 
+  * * * * 
+* * * * * 
+//
+
+public class Triangle_Rightside_Increasing_Pattern_Star {
+	
+	public static void main(String[] args) 
+	{
+		for(int row = 1; row<=5; row++)
+		{
+		for(int col = row; col<5; col++)
+		{
+			System.out.print("  ");
+		}
+		for(int col = 1; col<=row; col++)
+		{
+			System.out.print("* ");
+		}
+		System.out.println();		
+		}
+	}
+}
